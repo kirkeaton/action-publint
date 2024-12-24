f39fb87d2472060bda38a6bd16ce341e60402b0c
Kirk Eaton <contact@kirkeaton.ca>
2024-12-24 14:44:24 -0500

    try setting node packages as external

--- build.js
+++ build.js
@@ -5,6 +5,9 @@ import * as esbuild from 'esbuild';
 await esbuild.build({
   bundle: true,
   entryPoints: ['src/main.ts'],
+  external: [
+    'node:*',
+  ],
   format: 'esm',
   minify: true,
   outfile: 'dist/index.js',
