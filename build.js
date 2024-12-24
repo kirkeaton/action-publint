bc9af82a6228350b3ec4a39ddfa1c29d8ef9d69e
Kirk Eaton <contact@kirkeaton.ca>
2024-12-24 14:36:01 -0500

    fix: Switch build type to ESM

--- build.js
+++ build.js
@@ -4,8 +4,8 @@ import * as esbuild from 'esbuild';
 
 await esbuild.build({
   bundle: true,
-  entryPoints: ['src/main.mts'],
-  format: 'cjs',
+  entryPoints: ['src/main.ts'],
+  format: 'esm',
   minify: true,
   outfile: 'dist/index.js',
   platform: 'node',
