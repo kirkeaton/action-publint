7e1ad390a00c63ae1d12c794267e2ead4f015d52
Kirk Eaton <contact@kirkeaton.ca>
2025-02-02 16:07:19 -0500

    switch to esm

--- build.js
+++ build.js
@@ -5,9 +5,9 @@ import * as esbuild from 'esbuild';
 await esbuild.build({
   bundle: true,
   entryPoints: ['src/main.ts'],
-  format: 'cjs',
-  minify: true,
-  outfile: 'dist/index.cjs',
+  format: 'esm',
+  minify: false,
+  outfile: 'dist/index.js',
   platform: 'node',
   target: 'node20',
 });
